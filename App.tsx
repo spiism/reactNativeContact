3bb8416f613e7f35a20d2b56eee5d9141c2edce0
spiism <sp.iism@gmail.com>
2024-05-30 09:11:22 +1200

    add Search feature

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
-import React, {useEffect} from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import ContactList from './components/contactList/ContactList';
 import {Provider, useDispatch, useSelector} from 'react-redux';
 import store, {AppDispatch, RootState} from './store/store';
 import {fetchContacts} from './features/contactSlice';
+import Icon from 'react-native-vector-icons/AntDesign';
+import styles from './AppStyles';
 // import useFetch from './hook/useFetch';
 
 function AppContent(): React.JSX.Element {
@@ -16,13 +24,28 @@ function AppContent(): React.JSX.Element {
   // }, []);
 
   const dispatch = useDispatch<AppDispatch>();
-  const {data, loading, error} = useSelector(
-    (state: RootState) => state.contacts,
-  );
+  const {data, loading} = useSelector((state: RootState) => state.contacts);
+
+  const [searchVisible, setSearchVisible] = useState(false);
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
     dispatch(fetchContacts('https://reqres.in/api/users?page=2'));
   }, [dispatch]);
+
+  const handleSearch = () => {
+    setSearchVisible(!searchVisible);
+  };
+
+  const handleSearchChange = (text: string) => {
+    setSearchText(text);
+  };
+
+  const filteredData = data.filter(
+    contact =>
+      contact.first_name.toLowerCase().includes(searchText.toLowerCase()) ||
+      contact.last_name.toLowerCase().includes(searchText.toLowerCase()),
+  );
 
   return (
     <SafeAreaView style={{flex: 1}}>
@@ -41,9 +64,24 @@ function AppContent(): React.JSX.Element {
         }}>
         <Text>Menu</Text>
         <Text>Contact</Text>
-        <Text>Search</Text>
+        <TouchableOpacity onPress={handleSearch}>
+          <Icon name="search1" size={24} color="#000" />
+        </TouchableOpacity>
       </View>
-      <View style={{flex: 1}}>{!loading && <ContactList data={data} />}</View>
+
+      {searchVisible && (
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search by name"
+          value={searchText}
+          onChangeText={handleSearchChange}
+        />
+      )}
+      {/* <View style={{flex: 1}}>{!loading && <ContactList data={data} />}</View> */}
+
+      <View style={{flex: 1}}>
+        {!loading && <ContactList data={filteredData} />}
+      </View>
     </SafeAreaView>
   );
 }
---
+++ AppStyles.ts
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    backgroundColor: '#ffffff',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 16,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    margin: 16,
+    borderRadius: 4,
+  },
+});
+
+export default styles;
