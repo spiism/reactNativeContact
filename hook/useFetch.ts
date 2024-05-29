d5f21e14a028137e2cbc2cd2f4022774370a1b33
spiism <sp.iism@gmail.com>
2024-05-29 23:29:17 +1200

    add useFetch hook to fetch data

---
+++ hook/useFetch.ts
@@ -0,0 +1,25 @@
+import {useState} from 'react';
+
+const useFetch = (url: string) => {
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result.data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {data, loading, error, fetchData};
+};
+
+export default useFetch;
