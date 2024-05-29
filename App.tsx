469bf1e14c7e914b883f7f046a568aee0b645e0d
spiism <sp.iism@gmail.com>
2024-05-29 20:36:38 +1200

    wip/ add header

--- App.tsx
+++ App.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {SafeAreaView, View, Text} from 'react-native';
 import ContactList from './components/contactList/ContactList';
-import Header from './components/header/Header';
+// import Header from './components/header/Header';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Header />
-      <ContactList />
+      <View
+        style={{
+          height: 60,
+          backgroundColor: '#ffffff',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          flexDirection: 'row',
+          paddingHorizontal: 16,
+          shadowColor: '#000',
+          shadowOffset: {width: 0, height: 2},
+          shadowOpacity: 0.2,
+          elevation: 2,
+        }}>
+        <Text>Menu</Text>
+        <Text>Contact</Text>
+        <Text>Search</Text>
+      </View>
+      <View style={{flex: 1}}>
+        {/* <Header /> */}
+        <ContactList />
+      </View>
     </SafeAreaView>
   );
-  // return <Text>sdgdfg</Text>;
 }
 
 export default App;
