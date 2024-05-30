e46a8795ef71efa3d80be75d742a14b42632ef07
spiism <sp.iism@gmail.com>
2024-05-30 15:41:14 +1200

    config unit tests using @testing-library/react-native

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|tsx|js)$',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
+  },
 };
